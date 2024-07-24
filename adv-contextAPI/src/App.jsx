ecf135385f513055c42ca5f5f8444218d22dc921
Yash <singhyash0026@gmail.com>
2024-07-24 10:46:39 +0530

    initial-commit

---
+++ adv-contextAPI/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-orange-600 text-white m-5 p-4 text-5xl'>Another Advanced Topic in Context API.</h1>
+    </>
+  )
+}
+
+export default App
