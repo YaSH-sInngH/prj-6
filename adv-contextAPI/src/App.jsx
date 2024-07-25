d8d771e18e6c02eb7f3af55d6a17443fa30eb399
Yash <singhyash0026@gmail.com>
2024-07-25 16:44:47 +0530

    toggle-work-with-ContextAPI

--- adv-contextAPI/src/App.jsx
+++ adv-contextAPI/src/App.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './context/Theme'
+import ThemeButton from './components/ThemeButton'
+import Card from './components/Card'
 
 function App() {
+  const [themeMode, setThemeMode] = useState('light')
+  
+  const lightTheme = ()=>{
+    setThemeMode('light')
+  }
+  const darkTheme = ()=>{
+    setThemeMode('dark')
+  }
+
+  //Actual change in theme
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
-    <>
-      <h1 className='bg-orange-600 text-white m-5 p-4 text-5xl'>Another Advanced Topic in Context API.</h1>
-    </>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                <ThemeButton />
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
+        </div>
+    </div>
+    </ThemeProvider>
+
   )
 }
 
